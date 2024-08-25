a5c8edf974ae522b033c24b0b56955637f813887
vaishnavi <vaishnavit582@gmail.com>
2024-08-25 15:41:30 +0530

    Deployment

--- models/db.js
+++ models/db.js
@@ -9,10 +9,12 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306,  
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
 });
+
 
 pool.promise().query('SELECT 1 + 1 AS solution')
     .then(([rows, fields]) => {
